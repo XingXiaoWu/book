8a0f5e68572a23c83671dd483818d7908bbdbeb5
xing.wu <wuxing@bjca.org.cn>
2021-04-02 14:21:07 +0800

    书城页面

--- lib/pages/book_store.dart
+++ lib/pages/book_store.dart
@@ -1,12 +1,13 @@
 ///
 ///Author: Xing.Wu
 ///Date: 2021-04-02 10:57:14
-///LastEditTime: 2021-04-02 13:52:47
+///LastEditTime: 2021-04-02 14:19:57
 ///LastEditors: Xing.wu
 ///Description: 书城页面
 ///我是阿星，祝你幸福
 ///
 import 'package:flutter/material.dart';
+import 'package:flutter_book/app/app_color.dart';
 
 class BookStore extends StatefulWidget {
   @override
@@ -16,8 +17,41 @@ class BookStore extends StatefulWidget {
 class _BookStoreState extends State<BookStore> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("123"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("书城"),
+        leading: Center(
+          child: Text("左边"),
+        ),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+      ),
+      body: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            brightness: Brightness.light,
+            title: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TabBar(
+                labelColor: AppColor.darkGray,
+                labelStyle:
+                    TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                unselectedLabelColor: AppColor.gray,
+                indicatorColor: AppColor.secondary,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 3,
+                indicatorPadding: EdgeInsets.fromLTRB(8, 0, 8, 5),
+                tabs: [
+                  Tab(text: '精选'),
+                  Tab(text: '女生'),
+                  Tab(text: '男生'),
+                  Tab(text: '漫画'),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
