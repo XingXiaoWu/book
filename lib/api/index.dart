9531d8065fd2234bea2fffd84829b109878eb536
无星 <32910694@qq.com>
2021-04-04 00:43:32 +0800

    add

--- lib/api/index.dart
+++ lib/api/index.dart
@@ -1,19 +1,24 @@
-import 'package:dio/dio.dart';
-
 ///
 ///Author: Xing.Wu
 ///Date: 2021-04-02 18:03:54
-///LastEditTime: 2021-04-02 18:10:33
+///LastEditTime: 2021-04-04 00:42:50
 ///LastEditors: Xing.wu
 ///Description:
 ///我是阿星，祝你幸福
 ///
 
+import 'package:dio/dio.dart';
+import 'package:flutter_book/api/http.dart';
+import 'package:flutter_book/models/findModel.dart';
+
 class Api {
-  var dio = Dio(BaseOptions(
-      baseUrl: 'http://yuenov.com:15555',
-      connectTimeout: 5000,
-      receiveTimeout: 100000,
-      contentType: Headers.jsonContentType,
-      responseType: ResponseType.plain));
+  /// 获取http
+  static Http http = Http();
+
+  /// 发现页接口
+  static Future<FindModel> getFindFeature() async {
+    Response response = await http.dio.get('');
+    FindModel findModel = FindModel.fromJson(response.data);
+    return findModel;
+  }
 }
---
+++ lib/api/url.dart
@@ -0,0 +1,13 @@
+///
+///Author: Xing.Wu
+///Date: 2021-04-04 00:29:11
+///LastEditTime: 2021-04-04 00:30:13
+///LastEditors: Xing.wu
+///Description:
+///我是阿星，祝你幸福
+///
+
+class URLS {
+  static String baseUrl = 'http://yuenov.com:15555';
+  static String find = '/app/open/api/category/discovery';
+}
